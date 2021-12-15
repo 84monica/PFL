8851eb2007e20d9d9a9de6de1a387ca02063b39c
84monica <up201905753@fe.up.pt>
2021-12-15 20:31:27 +0000

    Changes to class Board

--- TP2/src/board.pl
+++ TP2/src/board.pl
@@ -1,18 +1,49 @@
-% REPRESENTAÇÃO DO TABULEIRO
+% REPRESENTAÇÃO DO ESTADO DO TABULEIRO
 
 % Sem peça - 0
 % Player 1 - 1
 % Player 2 - 2
 
-% Tabuleiro Inicial - 0
-% gameState(+Board, +State)
+% Tabuleiro Inicial
+startBoard(
+    [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0],
+    [2, 0, 0, 0, 2],
+    [2, 2, 2, 2, 2]
+    ]
+).
+
+% Exemplo de Tabuleiro Final (Player 1 vencedor)
+endBoard1(
+    [
+    [2, 2, 2, 2, 2],
+    [2, 0, 0, 0, 0],
+    [0, 0, 0, 2, 0],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+    ]
+).
+
+% Exemplo de Tabuleiro Final (Player 2 vencedor)
+endBoard2(
+    [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [2, 0, 0, 0, 2],
+    [2, 2, 2, 2, 2]
+    ]
+).
+
+% Exemplo de Tabuleiro Intermédio
 gameState(
     [
-    [1, 1, 1, 1, 1]
-    [1, 0, 0, 0, 1]
-    [0, 0, 0, 0, 0]
-    [2, 0, 0, 0, 2]
-    [2, 2, 2, 2, 2]
-    ],
-    0
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 2, 0, 1, 0],
+    [0, 0, 2, 0, 2],
+    [2, 0, 2, 2, 2]
+    ]
 ).
